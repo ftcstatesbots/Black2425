6209906b1f8c44b20160d80e6f8fd90b05156372
pigedy100 <ruhosk@gmail.com>
2024-11-21 11:30:53 -0600

    Configure Dairy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.1.1-0.0.0"
+	id("dev.frozenmilk.teamcode") version "10.1.1-0.0.1"
+}
+
+repositories {
+	maven {
+		url = uri("https://repo.dairy.foundation/releases)")
+	}
+}
+
+dependencies{
+	implementation("dev.frozenmilk.dairy:Core:1.0.0")
+	implementation("dev.frozenmilk.dairy:Util:1.0.2")
+	implementation("dev.frozenmilk.mercurial:Mercurial:1.0.0")
 }
 
 ftc {
 	// use this to easily add more FTC libraries
-
 	// adds support for kotlin
 	kotlin
 }
